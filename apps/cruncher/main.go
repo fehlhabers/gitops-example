57e1eb8cb9ed07aea31d2b47429a01fa473bc00a
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2023-10-17 21:24:03 +0200

    Docker configuration

--- apps/cruncher/main.go
+++ apps/cruncher/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("I'm alive!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, I'm a service!")
+	})
+
+	app.Listen(":8080")
 }
